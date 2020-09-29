80de44c504d50a9022f8c677d5163abff1b54754
manopsee <manopsee@hotmail.com>
2020-09-29 20:23:51 +0700

    Add forgot password

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
+import { ColorSchemeName,Text } from 'react-native';
 
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
--- screens/TabLoginScreen.tsx
+++ screens/TabLoginScreen.tsx
@@ -1,10 +1,11 @@
 
 import React, { Component } from 'react';
-import { StyleSheet,TextInput,Image,Button,AsyncStorage,TouchableOpacity,Alert,SafeAreaView, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import { StyleSheet,TextInput,Image,Button,AsyncStorage,TouchableOpacity,Alert,SafeAreaView, ScrollView, KeyboardAvoidingView, Platform,Modal } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import API from '../API'
 import { Text, View } from '../components/Themed';
+import ImageSlider from 'react-native-image-slider';
 
 const api = API.create()
 
@@ -17,7 +18,28 @@ export default class TabLoginScreen extends Component {
     this.state = {
       user: '',
       password: '',
+      gallary: [],
+      modalForgot: false,
+
+      cardId: '',
+      telNo: '',
+      userCode: '',
+      newPassword: '',
     }
+  }
+
+  componentDidMount(){
+    this.setGallary()
+  }
+
+  setGallary(){
+    let gr = [
+      'http://ca-comil.net/images_slide/1.jpg',
+      'http://ca-comil.net/images_slide/2.jpg',
+      'http://ca-comil.net/images_slide/3.jpg',
+      'http://ca-comil.net/images_slide/4.jpg'
+    ]
+    this.setState({gallary: gr})
   }
 
   onSignin =async()=> {
@@ -51,24 +73,36 @@ export default class TabLoginScreen extends Component {
     // this.props.navigation.navigate('Root')
   }
 
+  forgotPassword(){
+    const {cardId,telNo,userCode,newPassword} =this.state
+    console.log(cardId,telNo,userCode,newPassword)
+    this.setState({modalForgot: false})
+  }
+
   render(){
-    return (
+    return (<>
+      <View style={{width: '100%', height: 60, backgroundColor: '#cc66ff'}}></View>
       <SafeAreaView style={{flex: 1}}>
-        {/* <ScrollView style={{flex: 1, backgroundColor: '#fff'}}> */}
+        
+        <ScrollView style={{flex: 1, backgroundColor: '#fff'}}>
         
           <View style={styles.container}>
-            <Image source={require('./images/logo.png')} style={styles.images} resizeMode="cover"/>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+            <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 40, paddingHorizontal: 50}}>
+              <Image source={require('./images/logo.png')} style={styles.images} resizeMode="cover"/>
+              <Text style={{fontSize: 30, marginLeft: 30, flex: 1}}>เข้าใช้งาน</Text>
+            </View>
             
-            <Text style={styles.title}>หมายเลขฌาปนกิจ</Text>
-            <TextInput
-              style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 , borderRadius: 5, padding: 5}}
-              onChangeText={text=> this.setState({user: text})}
-              value={this.state.user}
-              placeholder="หมายเลขฌาปนกิจ"
-            />
+            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+            <View style={{alignSelf: 'flex-start', paddingHorizontal: 50}}>
+              <Text style={styles.title}>หมายเลขฌาปนกิจ</Text>
+              <TextInput
+                style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 , borderRadius: 5, padding: 5}}
+                onChangeText={text=> this.setState({user: text})}
+                value={this.state.user}
+                placeholder="หมายเลขฌาปนกิจ"
+              />
 
-            <Text style={styles.title}>รหัสผ่าน</Text>
+              <Text style={styles.title}>รหัสผ่าน</Text>
               <TextInput
                 style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 , borderRadius: 5, padding: 5}}
                 onChangeText={text=> this.setState({password: text})}
@@ -76,8 +110,13 @@ export default class TabLoginScreen extends Component {
                 placeholder="รหัสผ่าน"
                 // autoFocus={true}
               />
+            </View>
             
             <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+
+            <TouchableOpacity onPress={()=> this.setState({modalForgot: true})} style={{paddingVertical: 10, paddingHorizontal: 20, marginBottom: 20}}>
+              <Text style={{fontSize: 18, color: '#000'}}>ลืมรหัสผ่าน ?</Text>
+            </TouchableOpacity>
 
             <TouchableOpacity onPress={()=> this.onSignin()} style={{paddingVertical: 10, paddingHorizontal: 30, backgroundColor: '#FF4B2B', borderRadius: 20}}>
               <Text style={{fontSize: 18, color: '#fff'}}>Sing In</Text>
@@ -88,16 +127,85 @@ export default class TabLoginScreen extends Component {
             </TouchableOpacity>
 
           </View>
+
+          <View style={{width: '100%', height: 200}}>
+            <ImageSlider 
+              loopBothSides
+              autoPlayWithInterval={5000}
+              images={this.state.gallary}
+            />
+          </View>
           
-         {/* </ScrollView> */}
+         </ScrollView>
+
+         <Modal
+        animationType="fade"
+        transparent={true}
+        visible={this.state.modalForgot}
+        onRequestClose={() => {
+          // Alert.alert("Modal has been closed.");
+        }}
+        >
+          <View style={styles.centeredView}>
+            {/* <View style={styles.modalView}> */}
+              <Image source={require('./images/logo.png')} style={styles.images} resizeMode="cover"/>
+              <Text style={[{textAlign: 'center', color: '#000099'}]}>สำหรับสมาชิก ที่เคยลงทะเบียนในระบบ และลืมรหัสผ่าน</Text>
+              <Text style={[{textAlign: 'center', color: '#000099'}]}>ต้องการรีเซตรหัสผ่านโปรดระบุข้อมูลให้ถูกต้อง</Text>
+              <Text style={[{textAlign: 'center', color: '#000099'}]}>ตามที่เคยลงทะเบียน!</Text>
+
+              <Text style={[styles.title,{marginTop: 20}]}>หมายเลข ปชช.</Text>
+              <TextInput
+                style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 , borderRadius: 5, padding: 5}}
+                onChangeText={text=> this.setState({cardId: text})}
+                value={this.state.cardId}
+                placeholder="หมายเลข ปชช."
+              />
+              <Text style={[styles.title,{marginTop: 20}]}>หมายเลขโทรศัพท์</Text>
+              <TextInput
+                style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 , borderRadius: 5, padding: 5}}
+                onChangeText={text=> this.setState({telNo: text})}
+                value={this.state.telNo}
+                placeholder="หมายเลขโทรศัพท์"
+              />
+              <Text style={[styles.title,{marginTop: 20}]}>หมายเลขฌาปนกิจ สสอท. (รหัสผู้ใช้)</Text>
+              <TextInput
+                style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 , borderRadius: 5, padding: 5}}
+                onChangeText={text=> this.setState({userCode: text})}
+                value={this.state.userCode}
+                placeholder="หมายเลขฌาปนกิจ สสอท. (รหัสผู้ใช้)"
+              />
+              <Text style={[styles.title,{marginTop: 20}]}>รหัสผ่านใหม่</Text>
+              <TextInput
+                secureTextEntry={true}
+                style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 , borderRadius: 5, padding: 5}}
+                onChangeText={text=> this.setState({newPassword: text})}
+                value={this.state.newPassword}
+                placeholder="รหัสผ่านใหม่"
+              />
+
+              <View style={{flexDirection: 'row', marginTop: 20}}>
+                <TouchableOpacity onPress={()=> this.setState({modalForgot: false,cardId: '',telNo: '',userCode: '',newPassword: ''})} style={{paddingVertical: 10, backgroundColor: '#FF4B2B', paddingHorizontal: 20, borderRadius: 5}}>
+                  <Text style={{fontSize: 18, color: '#fff'}}>ยกเลิก</Text>
+                </TouchableOpacity>
+                <View style={{width: 20}}></View>
+                <TouchableOpacity onPress={()=> this.forgotPassword()} style={{paddingVertical: 10, backgroundColor: '#007bff', paddingHorizontal: 20, borderRadius: 5}}>
+                  <Text style={{fontSize: 18, color: '#fff'}}>รีเซตรหัสผ่าน</Text>
+                </TouchableOpacity>
+              </View>
+          
+            {/* </View> */}
+          </View>
+        </Modal>
+
       </SafeAreaView>
+      </>
     );
   }
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
+      // flex: 1,
       alignItems: 'center',
       justifyContent: 'center',
     },
@@ -108,18 +216,39 @@ const styles = StyleSheet.create({
       width: 300
     },
     separator: {
-      marginVertical: 30,
+      marginVertical: 20,
       height: 1,
       width: '80%',
     },
     images: {
-      width: 180,
-      height: 180,
-      marginTop: 80
+      width: 50,
+      height: 50,
+      // marginTop: 80
     },
     btSignin: {
       padding: 5,
       backgroundColor: '#007bff'
-    }
+    },
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5
+    },
   });
   
